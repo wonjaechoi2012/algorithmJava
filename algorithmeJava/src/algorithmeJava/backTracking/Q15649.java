ed2a3dcb3f1a8063b74fc3bc05bf1a6457c26696
wonjaechoi <wonjaechoi2014@gmail.com>
2019-10-17 02:42:12 +0900

    algorithm - backtracking

---
+++ algorithmeJava/src/algorithmeJava/backTracking/Q15649.java
@@ -0,0 +1,46 @@
+package algorithmeJava.backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Q15649 {
+
+	static boolean[] visit = new boolean[10];
+	static List<Integer> ans = new ArrayList<>();
+	static int Len;
+	static int Cnt;
+	static StringBuilder sb = new StringBuilder();
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Len = Integer.parseInt(st.nextToken());
+		Cnt = Integer.parseInt(st.nextToken());
+		arr = new int[Cnt];
+		dfs(0);
+		System.out.println(sb);
+	}
+	
+	public static void dfs(int cnt) {
+		if(cnt == Cnt) {
+			for(int i=0; i<Cnt; i++)
+				sb.append(arr[i]).append(" ");
+			sb.append('\n');
+		}else {
+			for(int i=0;i<Len;i++) {
+				if(!visit[i]) {
+					visit[i]=true;
+					arr[cnt]=i+1;
+					dfs(cnt+1);
+					visit[i]=false;
+				}
+			}
+		}
+	}
+
+}
---
+++ algorithmeJava/src/algorithmeJava/backTracking/Q15652.java
@@ -0,0 +1,41 @@
+package algorithmeJava.backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Q15652 {
+
+	static boolean[] visit = new boolean[10];
+	static List<Integer> ans = new ArrayList<>();
+	static int Len;
+	static int Cnt;
+	static StringBuilder sb = new StringBuilder();
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Len = Integer.parseInt(st.nextToken());
+		Cnt = Integer.parseInt(st.nextToken());
+		arr = new int[Cnt];
+		dfs(0,0);
+		System.out.println(sb);
+	}
+
+	public static void dfs(int cur, int cnt) {
+		if (cnt == Cnt) {
+			for (int i = 0; i < Cnt; i++)
+				sb.append(arr[i]).append(" ");
+			sb.append('\n');
+		} else {
+			for (int i = cur; i < Len; i++) {
+				arr[cnt] = i + 1;
+				dfs(i,cnt + 1);
+			}
+		}
+	}
+}
